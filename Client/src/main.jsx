6cabfda9075b675243e2b106bd088020bf635dac
KANWAR717 <kanwartarsem@gmail.com>
2025-07-24 10:53:25 +0530

    Fix: corrected spelling mistake

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import { persistor, store } from './redux/store.js'
+import { persistor, store } from './redux/Store.js'
 import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
