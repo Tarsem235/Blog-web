a33564918d43be784dfe626203b0b0a9c7d4091e
KANWAR717 <kanwartarsem@gmail.com>
2025-07-24 17:38:11 +0530

    add uploads folder for static image hosting

--- Client/src/pages/Post.jsx
+++ Client/src/pages/Post.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
-import { BaseUrl } from "../Services/Endpoint";
+
 
 const SinglePost = () => {
   const { id } = useParams();
